d74fb5c22695c6c157b7bde1f8d33251b0ad5722
luispiteira <lcpiteira@gmail.com>
2023-02-19 02:40:51 +0000

    Try fix Azure Active Directory Authentication

--- src/main/java/com/meter/your/calories/UserService/UserServiceApplication.java
+++ src/main/java/com/meter/your/calories/UserService/UserServiceApplication.java
@@ -2,8 +2,9 @@ package com.meter.your.calories.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
